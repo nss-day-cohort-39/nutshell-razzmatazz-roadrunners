188f917e972050905cea2cc40b9f2b96143b5a6f
nickglovdev <nickglovdev@gmail.com>
2020-04-15 13:01:54 -0500

    Added the ability for the registration anchor text to displaly the form for registration.

--- src/scripts/registration/RegistrationForm.js
+++ src/scripts/registration/RegistrationForm.js
@@ -1,7 +1,10 @@
 import { saveUsers } from "./RegistrarionProvider.js"
+import { DisplayRegistrationText } from "./DisplayRegistrationText.js"
 
 const contentTarget = document.querySelector(".registration_container")
+const eventHub = document.querySelector("#container");
 
+// Adding data to our database.json file uppon the click of the correct id.
 contentTarget.addEventListener("click", clickEvent => {
     if(clickEvent.target.id === "registerButton") {
         const newRegistration = {
@@ -11,15 +14,20 @@ contentTarget.addEventListener("click", clickEvent => {
         }
 
         saveUsers(newRegistration)
-
     }
+})
+
+// Removing invisible class upon clicking the anchor link for registration
+eventHub.addEventListener("allRegistrationClicked", event => {
+    document.querySelector(".registration").classList.remove("invisible")
+    document.querySelector(".registrationButton").classList.remove("invisible")       
 })
 
 const render = () => {
     contentTarget.innerHTML = `
         <h1>Welcome to Nutshell Registration</h1>   
-        <a href="#">Register a new account</a>
-        <form action="" class="registration">
+        ${DisplayRegistrationText()}
+        <form action="" class="registration invisible">
             <fieldset class="registration_email">
                 <label for="registrationEmail">Email</label>
                 <input type="text" name="registrationEmail" id="registrationEmail">
@@ -32,15 +40,15 @@ const render = () => {
 
             <fieldset class="registration_password">
                 <label for="registrationPassword">Password</label>
-                <input type="text" name="registrationPassword" id="registrationPassword">
+                <input type="password" name="registrationPassword" id="registrationPassword">
             </fieldset>
 
             <fieldset class="registration_confirm_password">
                 <label for="registrationConfirmPassword">Confirm Password</label>
-                <input type="text" name="registrationConfirmPassword" id="registrationConfirmPassword">
+                <input type="password" name="registrationConfirmPassword" id="registrationConfirmPassword">
             </fieldset>         
         </form>
-        <button id="registerButton">Register</button>
+        <button id="registerButton" class="registrationButton invisible">Register</button>
     `
 
 }
